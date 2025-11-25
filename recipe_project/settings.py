10e5ff35369ec1027c7591d3b11e15039533d5da
andria.fraderick <andriafraderick02@gmail.com>
2025-11-25 22:12:42 +0530

    Fix database for Render deployment

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recipe_database',
+        'USER': 'root',
+        'PASSWORD': 'Admin@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
